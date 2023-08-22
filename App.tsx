22760b2255cde1640d0df93bf5be0580a8a084b0
weedeej <devildjoseph666@gmail.com>
2023-08-22 19:37:08 +0800

    Added paper provider

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
 import { Home } from '@/screens';
+import { PaperProvider, MD3DarkTheme } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
+  const theme = {
+    ...MD3DarkTheme,
+    fonts: {
+      ...MD3DarkTheme.fonts
+    }
+  }
   return (
     <SafeAreaProvider>
-      <Home/>
+      <PaperProvider theme={theme}>
+        <Home/>
+      </PaperProvider>
     </SafeAreaProvider>
   );
 }
