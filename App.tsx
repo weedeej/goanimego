9d395c00410ff8dfa63b63719fded37d5d7291b0
weedeej <devildjoseph666@gmail.com>
2023-08-22 23:42:47 +0800

    Add drawer items

--- App.tsx
+++ App.tsx
@@ -1,8 +1,29 @@
 import { Colors } from '@/constants';
 import { Home } from '@/screens';
+import { Feather } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
 import { PaperProvider, MD3DarkTheme } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+const DrawerItems = [
+  {
+      name: 'Profile',
+      icon: <Feather name="user"/>
+  },
+  {
+      name: 'Settings',
+      icon: <Feather name="settings"/>
+  },
+  {
+      name: 'Playlist',
+      icon: <Feather name="play-circle"/>
+  },
+  {
+      name: '',
+      icon: <Feather name="settings"/>
+  }
+] // TODO: add drawer items
+
 export default function App() {
   const theme = {
     ...MD3DarkTheme,
